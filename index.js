c8ce9648bb83a4364fd3fa6cc376aa1e22e0ccda
Talles L <talleslasmar@gmail.com>
2014-10-12 11:47:58 -0300

    considering lines that are just the emoji

--- index.js
+++ index.js
@@ -2,26 +2,35 @@ var url = require('url')
 var emojis = require('./emojis')
 
 exports.replaceWithUnicode = function (input) {
-  var words = input.split(' ')
-  for (var i = 0; i < words.length; ++i) words[i] = unicodeForEmoji(words[i])
-  return words.join(' ')
+  return replace(unicodeForEmoji, input)
 }
 
 exports.replaceWithHtml = function (input, baseUrl) {
-  var words = input.split(' ')
-  for (var i = 0; i < words.length; ++i) words[i] = htmlForEmoji(words[i], baseUrl)
-  return words.join(' ')
+  return replace(htmlForEmoji, input, baseUrl)
 }
 
-function unicodeForEmoji(word) {
-  return emojis[word] === undefined ? word : emojis[word]
+function replace (fn, input, baseUrl) {
+  var output = input
+  output = replaceTokens(fn, output, ' ', baseUrl)
+  output = replaceTokens(fn, output, '\n', baseUrl)
+  return output
 }
 
-function htmlForEmoji (word, baseUrl) {
-  if (emojis[word] === undefined) return word
+function replaceTokens (fn, input, separator, baseUrl) {
+  var tokens = input.split(separator)
+  for (var i = 0; i < tokens.length; ++i) tokens[i] = fn(tokens[i], baseUrl)
+  return tokens.join(separator)
+}
+
+function unicodeForEmoji(token) {
+  return emojis[token] === undefined ? token : emojis[token]
+}
+
+function htmlForEmoji (token, baseUrl) {
+  if (emojis[token] === undefined) return token
   else {
-      var emojiName = word.replace(/:/g, '')
+      var emojiName = token.replace(/:/g, '')
       var src = (baseUrl ? url.resolve(baseUrl, emojiName)  : emojiName) + '.png'
-      return '<img class="emoji" width="20" height="20" align="absmiddle" src="' + src + '" alt="' + word + '" />'
+      return '<img class="emoji" width="20" height="20" align="absmiddle" src="' + src + '" alt="' + token + '" />'
   }
 }
