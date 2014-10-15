267f6311b5f123b8d92055882b65add5f14224bd
Talles L <talleslasmar@gmail.com>
2014-10-15 01:34:23 +0000

    fixed, refactored and commented the code

--- index.js
+++ index.js
@@ -2,35 +2,75 @@ var url = require('url')
 var emojis = require('./emojis')
 
 exports.replaceWithUnicode = function (input) {
-  return replace(unicodeForEmoji, input)
+  return replace(input, unicodeForEmoji)
 }
 
 exports.replaceWithHtml = function (input, baseUrl) {
-  return replace(htmlForEmoji, input, baseUrl)
+  return replace(input, htmlForEmoji, baseUrl)
 }
 
-function replace (fn, input, baseUrl) {
-  var output = input
-  output = replaceTokens(fn, output, ' ', baseUrl)
-  output = replaceTokens(fn, output, '\n', baseUrl)
-  return output
+function replace (input, fn, baseUrl) {
+  
+  // tokens split by colon
+  var tokens = input.split(':')
+  
+  // parsed tokens
+  var parsed = [ ]
+  
+  // flag indicating if previous iteration token is an emoji
+  var emojiBefore = true;
+  
+  // for each token
+  for (var i = 0; i < tokens.length; ++i) {
+
+    // current iteration token
+    var token = tokens[i]
+
+    // flag indicating if current iteration token is an emoji
+    var emojiCurrent = isEmoji(token)
+    
+    // if we are not dealing with an emoji
+    if (!emojiBefore && !emojiCurrent) {
+      
+      // adds the colon back
+      parsed.push(':')
+      
+    }
+    
+    // if current iteration token is an emoji
+    if (emojiCurrent) {
+      
+      // parses the emoji and push it to the parsed array
+      parsed.push(fn(tokens[i], baseUrl))
+      
+    }
+    
+    // if current iteration token isn't an emoji
+    else {
+      
+      // push it without any parsing
+      parsed.push(tokens[i])
+      
+    }
+    
+    // setting current flag as before flag
+    emojiBefore = emojiCurrent
+    
+  }
+  
+  // joining the parsed tokens and returning it
+  return parsed.join('')
 }
 
-function replaceTokens (fn, input, separator, baseUrl) {
-  var tokens = input.split(separator)
-  for (var i = 0; i < tokens.length; ++i) tokens[i] = fn(tokens[i], baseUrl)
-  return tokens.join(separator)
+function isEmoji(token) {
+  return emojis[token] !== undefined
 }
 
 function unicodeForEmoji(token) {
-  return emojis[token] === undefined ? token : emojis[token]
+  return emojis[token]
 }
 
 function htmlForEmoji (token, baseUrl) {
-  if (emojis[token] === undefined) return token
-  else {
-      var emojiName = token.replace(/:/g, '')
-      var src = (baseUrl ? url.resolve(baseUrl, emojiName)  : emojiName) + '.png'
-      return '<img class="emoji" width="20" height="20" align="absmiddle" src="' + src + '" alt="' + token + '" />'
-  }
+  var src = (baseUrl ? url.resolve(baseUrl, token)  : token) + '.png'
+  return '<img class="emoji" width="20" height="20" align="absmiddle" src="' + src + '" alt="' + token + '" />'
 }
